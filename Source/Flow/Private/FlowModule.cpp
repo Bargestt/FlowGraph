bf0afd6ad42d7adf5a8009e1e04357661b8fd66f
Vasilii Bulgakov <bulgakovbarg@gmail.com>
2026-04-12 23:31:38 +0700

    Addded versioning to FlowNode and FlowAsset

--- Source/Flow/Private/FlowModule.cpp
+++ Source/Flow/Private/FlowModule.cpp
@@ -2,7 +2,12 @@
 
 #include "FlowModule.h"
 
+#include "FlowVersion.h"
 #include "Modules/ModuleManager.h"
+
+const FGuid FFlowVersion::GUID(0x2e8af3ae, 0xde0944e2, 0xaf936817, 0x9250369c);
+
+static FCustomVersionRegistration GRegisterFlowCustomVersion(FFlowVersion::GUID, FFlowVersion::LatestVersion, TEXT("FlowVersion"));
 
 void FFlowModule::StartupModule()
 {
